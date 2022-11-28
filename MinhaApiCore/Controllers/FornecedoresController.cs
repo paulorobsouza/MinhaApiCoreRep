bb0100423ce87e7cc9e21f8342cd168853aaee20
paulorobsouza <pauloestudosweb@gmail.com>
2022-11-28 08:04:56 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaApiCore/Controllers/FornecedoresController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinhaApiCore.Model;
+
+namespace MinhaApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FornecedoresController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public FornecedoresController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Fornecedores
+        [HttpGet]
+        public IEnumerable<Fornecedor> GetFornecedores()
+        {
+            return _context.Fornecedores;
+        }
+
+        // GET: api/Fornecedores/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetFornecedor([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(fornecedor);
+        }
+
+        // PUT: api/Fornecedores/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFornecedor([FromRoute] Guid id, [FromBody] Fornecedor fornecedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != fornecedor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fornecedor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FornecedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Fornecedores
+        [HttpPost]
+        public async Task<IActionResult> PostFornecedor([FromBody] Fornecedor fornecedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Fornecedores.Add(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFornecedor", new { id = fornecedor.Id }, fornecedor);
+        }
+
+        // DELETE: api/Fornecedores/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFornecedor([FromRoute] Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var fornecedor = await _context.Fornecedores.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fornecedores.Remove(fornecedor);
+            await _context.SaveChangesAsync();
+
+            return Ok(fornecedor);
+        }
+
+        private bool FornecedorExists(Guid id)
+        {
+            return _context.Fornecedores.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MinhaApiCore/Controllers/ValuesController.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MinhaApiCore.Controllers
+{
+    [Route("api/[controller]")]
+   
+    public class ValuesController : MainController
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> ObterTodos()
+        {
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length < 5000)
+                return BadRequest();
+
+            return valores;
+        }
+
+        public ActionResult ObterResultado()
+        {
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length < 5000)
+                return CustomResponse();
+
+            return CustomResponse(valores);
+        }
+
+
+        [HttpGet("obter-valores")]
+        public IEnumerable<string> ObterValores()
+        {
+            var valores = new string[] { "value1", "value2" };
+
+            if (valores.Length < 5000)
+                return null;
+
+            return valores;
+        }
+
+
+        // GET api/values/obter-por-id/5
+        [HttpGet("obter-por-id/{id:int}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        //[ApiConventionMethod(typeof(DefaultApiConventions),nameof(DefaultApiConvetions.Post))]
+        public ActionResult Post(Product value)
+        {
+            if (value.Id == 0)
+                return BadRequest();
+
+            //ok banco
+            //return Ok(value);
+
+            //return CreatedAtAction("Post", value);
+
+            return CreatedAtAction(nameof(Post), value);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put([FromRoute]int id , [FromForm]Product value)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            if (id != value.Id) return NotFound();
+            
+            //ok banco
+            return Ok(value);
+
+            //return CreatedAtAction("Post", value);
+            //retur NoContent(); codigo 204 sem conteúdo
+            
+        }
+
+
+        // PUT api/values/5
+        
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete([FromQuery]int id)
+        {
+        }
+
+       
+    }
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperaValida())
+            {
+                return Ok(new
+                {
+                    sucess = true,
+                    data = result
+                }) ;
+            }
+            return BadRequest(new
+            {
+                sucess = false,
+                errors = ObterErros()
+            });
+        }
+
+        public bool OperaValida()
+        {
+            //validações
+
+            return true;
+        }
+
+        protected string ObterErros()
+        {
+            return "";
+
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+    }
+}
